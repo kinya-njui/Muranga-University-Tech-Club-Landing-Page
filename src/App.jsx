a1847328c237c27f8fa1a7ab915e6e7e52bf0c26
kinya-njui <kinyanjuicharlesm@gmail.com>
2024-09-30 22:47:22 +0300

    Updating the TrackPages Path

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import "./App.css";
 import Header from "./components/Header/header";
 import Footer from "./components/Footer/Footer";
-// // import Hero from "./components/Hero/Hero";
-// // import Title from "./components/Title/Title";
-// // import Leadership from "./components/Leadership/Leadership";
-// // import AboutUs from "./components/AboutUs/AboutUs";
-// // import Testimonies from "./components/Testimonies/Testimonies";
-// // import Tracks from "./components/Tracks/Tracks";
-// // import Events from "./components/Events/Events";
 import Home from "./Pages/Home/Home";
-import EventsPage from "./Pages/Eventspage/EventsPage";
+import EventsPage from "./Pages/EventsPage/EventsPage";
 import LeaderShip from "./Pages/Leadership/Leadership";
 import Tracks from "./Pages/Tracks/Tracks";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -27,15 +20,6 @@ function App() {
         </Routes>
       </BrowserRouter>
       <Footer />
-      {/* <Header />
-      <Hero />
-      <AboutUs />
-      <Testimonies />
-      <Title />
-      <Leadership />
-      <Events />
-      <Tracks />
-      <Footer /> */}
     </>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,33 +22,3 @@ function GithubLink() {
 }
 
 export default Footer;
-
-// import './footer.css';
-
-// function PageFooter () {
-//     return (
-//         <div className="footer section">
-//                  <p>&copy; 2024 Urban Haven Apartments. All rights reserved.</p>
-
-//         </div>
-//         )
-// }
-
-// // function GithubLink() {
-// //     return (
-// //         <div class="github-profile-link">
-// //     <a href="https://github.com/kinya-njui" class="github-profile-link">charles kinyanjui</a>
-// // </div>
-// //     )
-// // }
-
-// // function Footer() {
-// //     return (
-// //         <Footer>
-// //             <PageFooter/>
-// //             {/* <GithubLink/> */}
-// //         </Footer>
-// //     )
-// // }
-
-// export default PageFooter
