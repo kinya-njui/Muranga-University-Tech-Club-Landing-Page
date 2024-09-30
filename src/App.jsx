0b0eaa37db10fac66e9a849b9da565e7fcba6403
kinya-njui <kinyanjuicharlesm@gmail.com>
2024-09-30 13:06:36 +0300

    creat Track section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Title from "./components/Title/Title";
 import Catalogue from "./components/Catalogue/Catalogue";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Testimonies from "./components/Testimonies/Testimonies";
+import Tracks from "./components/Tracks/Tracks";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Title />
       <Catalogue />
       <Testimonies />
+      <Tracks />
       <Footer />
     </>
   );
---
+++ src/components/Tracks/Tracks.jsx
@@ -0,0 +1,33 @@
+import "./Tracks.css";
+import tracks from "../../data/tracks";
+import Title from "../Title/Title";
+
+function TrackCard({ trackImage, trackName, trackDescription }) {
+  return (
+    <div className="track-card">
+      <img src={trackImage} alt={trackName} className="track-card-image" />
+      <h4 className="track-card-name">{trackName}</h4>
+      <p className="track-card-description">{trackDescription}</p>
+    </div>
+  );
+}
+
+function Tracks() {
+  return (
+    <section>
+      <Title maintitle="Explore Our Tracks" subtitle="Choose Your Path" />
+      <div className="tracks-container">
+        {tracks.map((track, index) => (
+          <TrackCard
+            key={index}
+            trackImage={track.trackImage}
+            trackName={track.trackName}
+            trackDescription={track.trackDescription}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Tracks;
