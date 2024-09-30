d21284c9c67d17e849830db248db4c562b9e049a
kinya-njui <kinyanjuicharlesm@gmail.com>
2024-09-30 14:28:32 +0300

    Updating the Header path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/Header/Header";
+import Header from "./components/Header/header";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Title from "./components/Title/Title";
